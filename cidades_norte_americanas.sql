cc67c3a901327a998ed92bc8987be58ac456f9c0
Nalu <ana.lucia.cavalcante.sirino@gmail.com>
2025-06-30 09:55:38 -0300

    criando tabela de cidades

---
+++ cidades_norte_americanas.sql
@@ -0,0 +1,127 @@
+create database cidadesAmericanas;
+
+use cidadesAmericanas;
+
+create table
+    cidades_norte_americanas (
+        cidade varchar(200),
+        país varchar(200),
+        populacao numeric(15),
+        latitude numeric(15),
+        longitude numeric(15)
+    );
+
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Guadalajara",
+        "Mexico",
+        1500800,
+        20.659699,
+        -103.349609
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Toronto",
+        "Canada",
+        2795060,
+        43.653226,
+        -79.383184
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Houston",
+        "United States",
+        2195914,
+        29.760427,
+        -95.369803
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "New York",
+        "United States",
+        8405837,
+        40.712784,
+        -74.005941
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Philadelphia",
+        "United States",
+        1553165,
+        39.952584,
+        -75.165222
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    ("Havana", "Cuba", 2106146, 23.05407, -82.345189)
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Mexico City",
+        "Mexico",
+        8555500,
+        19.432608,
+        -99.133208
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Phoenix",
+        "United States",
+        1513367,
+        33.448377,
+        -112.074037
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Los Angeles",
+        "United States",
+        3884307,
+        34.052234,
+        -118.243685
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Ecatepec de Morelos",
+        "Mexico",
+        1742000,
+        19.601841,
+        -99.050674
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Montreal",
+        "Canada",
+        1717767,
+        45.501689,
+        -73.567256
+    )
+insert into
+    cidades_norte_americanas (cidade, país, populacao, latitude, longitude)
+values
+    (
+        "Chicago",
+        "United States",
+        2718782,
+        41.878114,
+        -87.629798
+    )
